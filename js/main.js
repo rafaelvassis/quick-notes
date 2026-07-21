86a1175fb9e752b4474bda397215cdc31e78c266
Rafael Vasconcelos <955502@sga.pucminas.br>
2026-07-21 08:05:44 -0300

    adiciona elementos da página no arquivo main.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/* Elementos da página */
+
+const formulario = document.getElementById("form-nota");
+const inputTitulo = document.getElementById("input-titulo");
+const selectCategorias = document.getElementById("select-categoria");
+const textAreaDescricao = document.getElementById("textarea-descricao");
+const btnSalvar = document.getElementById("btn-salvar");
+
+const areaMensagens = document.getElementById("area-mensagens");
+
+const listaNotas = document.getElementById("lista-notas");
