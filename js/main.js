5c05ec5a5afb507d71c17660429451d60f84a6d8
Rafael Vasconcelos <955502@sga.pucminas.br>
2026-07-21 09:16:05 -0300

    refatora a estrutura do código em main.js, adicionando a classe Nota e melhorando a lógica de adição de notas

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-/* Elementos da página */
-
+// Elementos da página
 const formulario = document.getElementById("form-nota");
 const inputTitulo = document.getElementById("input-titulo");
 const selectCategorias = document.getElementById("select-categoria");
@@ -9,3 +8,59 @@ const btnSalvar = document.getElementById("btn-salvar");
 const areaMensagens = document.getElementById("area-mensagens");
 
 const listaNotas = document.getElementById("lista-notas");
+
+// Classe 
+class Nota {
+  constructor(id, titulo, categoria, descricao, dataCriacao) {
+    this.id = id;
+    this.titulo = titulo;
+    this.categoria = categoria;
+    this.descricao = descricao;
+    this.dataCriacao = dataCriacao;
+  }
+}
+
+let notas = [];
+
+btnSalvar.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let titulo = inputTitulo.value;
+  let categoria = selectCategorias.value;
+  let descricao = textAreaDescricao.value;
+  let dataCriacao = new Date().toLocaleDateString("pt-BR");
+
+  let novaNota = new Nota(
+    crypto.randomUUID(),
+    titulo,
+    categoria,
+    descricao,
+    dataCriacao,
+  );
+
+  notas.push(novaNota);
+
+  inputTitulo.value = "";
+  selectCategorias.value = "sem categoria";
+  textAreaDescricao.value = "";
+
+
+  listaNotas.innerHTML += `
+        <article class="card-nota">
+        <div class="card-header">
+            <h3 class="card-titulo">${novaNota.titulo}</h3>
+            <span class="card-categoria">Categoria: ${novaNota.categoria}</span>
+            <span class="card-data">Criado em: ${novaNota.dataCriacao}</span>
+        </div>
+        
+        <p class="card-descricao">
+            ${novaNota.descricao}
+        </p>
+        
+        <div class="card-footer">
+            <button class="btn-alterar" data-id="${novaNota.id}">Alterar</button>
+            <button class="btn-deletar" data-id="${novaNota.id}">Excluir</button>
+        </div>
+        </article>
+    `;
+});
